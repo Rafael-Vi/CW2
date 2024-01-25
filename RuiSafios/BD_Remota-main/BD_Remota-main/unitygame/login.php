82e8d47cfb67c69d4dc0d9275caea6fc6542a15f
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-01-25 08:45:44 +0000

    Update login

--- RuiSafios/BD_Remota-main/BD_Remota-main/unitygame/login.php
+++ RuiSafios/BD_Remota-main/BD_Remota-main/unitygame/login.php
@@ -13,12 +13,12 @@
 	$dataregisto=date("Y-m-d H:i:s");
 
 	//Verificar se nome de utilizador já existe na BD
-	$qVerificarNome = "SELECT nome, password, salt, pontuacao FROM jogador WHERE nome LIKE '" . $nomeJogador ."';";
+	$qVerificarNome = "SELECT nome, password, salt, pontuacao, is_verified FROM jogador WHERE nome LIKE '" . $nomeJogador ."';";
 	$verificaNome = mysqli_query($ligacao, $qVerificarNome) or die ("#2: a query verifica nome falhou"); //Erro #2 = Não foi possivel verificar o nome do utilizador 
 	$numResultados = mysqli_num_rows($verificaNome);
 	if($numResultados != 1)
 	{
-		echo "5: Utilizador não está registado ou mais do que um utilizador registado com o mesmo nome. Contacte o administrador"; // Erro #5 = Numero de utilizadores registados != 1
+		echo "5: Utilizador não está registado ou mais do que um utilizador registado com o mesmo nome."; // Erro #5 = Numero de utilizadores registados != 1
 		exit();
 	}
 
@@ -33,6 +33,15 @@
 	{
 		echo "6: Password introduzida não está correta";// Erro #6 as password não são iguais
 		exit();
+	}
+
+	$isVerified = $VerificarDadosRegistados["is_verified"];
+	if ($isVerified == 1) {
+		// User account is verified
+		echo 0, "\t", $VerificarDadosRegistados["pontuacao"], "\t", "Account is verified";
+	} else {
+		// User account is not verified
+		echo 0, "\t", $VerificarDadosRegistados["pontuacao"], "\t", "Account is not verified";
 	}
 	echo 0 , "\t" , $VerificarDadosRegistados["pontuacao"];
 ?>
